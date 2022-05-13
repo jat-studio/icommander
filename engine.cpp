86131452e85f96ebdf22888fb9d74b8e479bed2f
Dmitry <jat-studio@yandex.ru>
2022-05-14 00:22:23 +0400

    Fix spaces, add stars.

--- engine.cpp
+++ engine.cpp
@@ -18,89 +18,89 @@ using namespace std;
 /*#####################Class Scene implementation###################*/
 // initialization count of textures
 void ClassScene::InitTextures(unsigned short int textures_count){
-  // count of textures
-  ClassScene::textures_count = textures_count;
-  // index texture of tiles
-  //texture_tiles = new GLuint[texture_tiles];
+    // count of textures
+    ClassScene::textures_count = textures_count;
+    // index texture of tiles
+    //texture_tiles = new GLuint[texture_tiles];
 }
 
 // set 2d mode
 void ClassScene::setOrthoProjection(GLsizei Width, GLsizei Height){
-  glColor3f(0.0, 0.0, 0.0);
-  glDisable(GL_BLEND);
+    glColor3f(0.0, 0.0, 0.0);
+    glDisable(GL_BLEND);
 
-  glMatrixMode(GL_PROJECTION);
-  glPushMatrix();
-  glLoadIdentity();
-  gluOrtho2D(0, 0, Width, Height);
-  glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_PROJECTION);
+    glPushMatrix();
+    glLoadIdentity();
+    gluOrtho2D(0, 0, Width, Height);
+    glMatrixMode(GL_MODELVIEW);
 
-  glPushMatrix();
-  glLoadIdentity();
+    glPushMatrix();
+    glLoadIdentity();
 }
 
 // set 3d mode
 void ClassScene::setPerspectiveProjection(){
-  glPopMatrix();
+    glPopMatrix();
 
-  glMatrixMode(GL_PROJECTION);
-  glPopMatrix();
-  glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_PROJECTION);
+    glPopMatrix();
+    glMatrixMode(GL_MODELVIEW);
 
-  glColor3f(1.0, 1.0, 1.0);
-  glEnable(GL_BLEND);
+    glColor3f(1.0, 1.0, 1.0);
+    glEnable(GL_BLEND);
 }
 
 // output string
 void ClassScene::DrawStaticString(float x, float y, float z, void *font, string input){
-  glRasterPos3f(x, y, z);
-  unsigned short int len = input.length();
-  for(unsigned short int i = 0; i < len; i++){
-    glutBitmapCharacter(font, input[i]);
-  }
+    glRasterPos3f(x, y, z);
+    unsigned short int len = input.length();
+    for(unsigned short int i = 0; i < len; i++){
+        glutBitmapCharacter(font, input[i]);
+    }
 }
 
 // loading texture by filename
 void ClassScene::LoadTextureImage(const char *texture_name, GLuint texture){
-  // parameters of image
-  int width;
-  int height;
-  int bpp;
-  unsigned int type;
-  unsigned char *image_data;
+    // parameters of image
+    int width;
+    int height;
+    int bpp;
+    unsigned int type;
+    unsigned char *image_data;
 
-  // loading texture
-  ilLoad(IL_TGA, texture_name);
-  // processing of errors
-  int err = ilGetError();
-  if (err != IL_NO_ERROR){
-    const char *strError = iluErrorString(err);
-    cout << "Error loading texture!\n" << texture_name << "\n" << strError << "\n";
-    exit(EXIT_FAILURE);
-  }
-  // getting parameters of image
-  width  = ilGetInteger(IL_IMAGE_WIDTH);
-  height = ilGetInteger(IL_IMAGE_HEIGHT);
-  bpp    = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
-  // getting data of image
-  image_data = ilGetData();
-  // determination type of image
-  switch (bpp){
-    case 1:
-      type = GL_RGB8;
-    break;
-    case 3:
-      type = GL_RGB;
-    break;
-    case 4:
-      type = GL_RGBA;
-    break;
-  }
-  // loading texture into memory
-  glBindTexture(GL_TEXTURE_2D, texture);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
-  glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-  gluBuild2DMipmaps(GL_TEXTURE_2D, bpp, width, height, type, GL_UNSIGNED_BYTE, image_data);
+    // loading texture
+    ilLoad(IL_TGA, texture_name);
+    // processing of errors
+    int err = ilGetError();
+    if (err != IL_NO_ERROR){
+        const char *strError = iluErrorString(err);
+        cout << "Error loading texture!\n" << texture_name << "\n" << strError << "\n";
+        exit(EXIT_FAILURE);
+    }
+    // getting parameters of image
+    width  = ilGetInteger(IL_IMAGE_WIDTH);
+    height = ilGetInteger(IL_IMAGE_HEIGHT);
+    bpp    = ilGetInteger(IL_IMAGE_BYTES_PER_PIXEL);
+    // getting data of image
+    image_data = ilGetData();
+    // determination type of image
+    switch (bpp){
+        case 1:
+            type = GL_RGB8;
+        break;
+        case 3:
+            type = GL_RGB;
+        break;
+        case 4:
+            type = GL_RGBA;
+        break;
+    }
+    // loading texture into memory
+    glBindTexture(GL_TEXTURE_2D, texture);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    gluBuild2DMipmaps(GL_TEXTURE_2D, bpp, width, height, type, GL_UNSIGNED_BYTE, image_data);
 }
 
 // loading textures
@@ -114,11 +114,11 @@ void ClassScene::LoadTextures(vector<string> textures_list){
     // loading textures
     unsigned short int val = 0;
     for(unsigned short int i = 1; i < textures_list.size(); i += 2){
-      ClassScene::TextureManager.insert(pair<string, unsigned short int>(textures_list[i], val));
-      string str = textures_list[0];
-      str += textures_list[i + 1];
-      ClassScene::LoadTextureImage(str.c_str(), ClassScene::texture_tiles[val]);
-      val++;
+        ClassScene::TextureManager.insert(pair<string, unsigned short int>(textures_list[i], val));
+        string str = textures_list[0];
+        str += textures_list[i + 1];
+        ClassScene::LoadTextureImage(str.c_str(), ClassScene::texture_tiles[val]);
+        val++;
     }
     // enabling textures
     glEnable(GL_TEXTURE_2D);
@@ -131,162 +131,175 @@ void ClassScene::ClearTextures(){
 
 // painting Scene
 void ClassScene::Draw(){
-  glClear(GL_COLOR_BUFFER_BIT);
-  glLoadIdentity();
-  gluLookAt(ClassScene::camera_xpos, ClassScene::camera_ypos, -6.0,
-            ClassScene::camera_xpos, ClassScene::camera_ypos, 1.0,
-            0.0, 1.0, 0.0);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glLoadIdentity();
+    gluLookAt(
+        ClassScene::camera_xpos, ClassScene::camera_ypos, -6.0,
+        ClassScene::camera_xpos, ClassScene::camera_ypos, 1.0,
+        0.0, 1.0, 0.0
+    );
 
-  // painting world
-  glScalef(scale, scale, 0.0);
+    // painting world
+    glScalef(scale, scale, 0.0);
 
-  glutSwapBuffers();
+    ClassScene::DrawStars();
+
+    glutSwapBuffers();
+}
+
+void ClassScene::DrawStars(){
+    glBegin(GL_POINTS);
+    glColor3f(1, 1, 1);
+    for (int i = 0; i < 100; i++){
+        glVertex3f(rand() % 130 - 65, rand() % 70 - 35, 0);
+    }
+    glEnd();
 }
 
 // repainting OpenGL by reshape window
 void ClassScene::Reshape(GLsizei Width, GLsizei Height){
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
-  glEnable(GL_BLEND);
-  glViewport(0, 0, Width, Height);
-  gluPerspective(45.0, (GLfloat) Width / (GLfloat) Height, 0.1, 100.0);
-  glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+    glEnable(GL_BLEND);
+    glViewport(0, 0, Width, Height);
+    gluPerspective(45.0, (GLfloat) Width / (GLfloat) Height, 0.1, 100.0);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 // destructor
 ClassScene::~ClassScene(){
-  ClassScene::TextureManager.clear();
+    ClassScene::TextureManager.clear();
 }
 
 /*#####################Class Console implementation###################*/
 // constructor
 ClassConsole::ClassConsole(){
-  for(unsigned short int i = 0; i <= 13; i++){
-    ClassConsole::console_str[i] = "";
-  }
-  ClassConsole::dualcommands["goto"] = &ClassConsole::Goto_x_y;
+    for(unsigned short int i = 0; i <= 13; i++){
+        ClassConsole::console_str[i] = "";
+    }
+    ClassConsole::dualcommands["goto"] = &ClassConsole::Goto_x_y;
 }
 
 // painting Console
 void ClassConsole::Draw(ClassScene &Object, unsigned short int console, unsigned short int wnd){
-  glutSetWindow(console);
-  if (!ClassConsole::visible){
-    glutHideWindow();
-  }
-  else{
-    glutShowWindow();
-  }
+    glutSetWindow(console);
+    if (!ClassConsole::visible){
+        glutHideWindow();
+    }
+    else{
+        glutShowWindow();
+    }
 
-  glClear(GL_COLOR_BUFFER_BIT);
+    glClear(GL_COLOR_BUFFER_BIT);
 
-  glLoadIdentity();
+    glLoadIdentity();
 
-  glColor3f(0.0, 0.0, 0.0);
+    glColor3f(0.0, 0.0, 0.0);
 
-  // drawing fps
-  ClassConsole::fps++;
-  ClassConsole::t = glutGet(GLUT_ELAPSED_TIME);
-  if (ClassConsole::t - ClassConsole::dt > 1000){
-    ClassConsole::console_str[0] = "FPS:" + Int_To_Str((int)(ClassConsole::fps * 1000.0 / (ClassConsole::t - ClassConsole::dt)));
-    ClassConsole::dt = ClassConsole::t;
-    ClassConsole::fps = 0;
-  }
-  ClassConsole::str_current_pos = ClassConsole::str_start_pos;
-  Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, ClassConsole::console_str[0]);
-  ClassConsole::str_current_pos -= ClassConsole::str_height;
-
-  // drawing coordinates
-  ClassConsole::console_str[1] = "X: ";
-  ClassConsole::console_str[1] += Int_To_Str(Object.xpos);
-  ClassConsole::console_str[1] += "; Y: ";
-  ClassConsole::console_str[1] += Int_To_Str(Object.ypos);
-  Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, ClassConsole::console_str[1]);
-  ClassConsole::str_current_pos -= ClassConsole::str_height;
-
-  for (unsigned short int i = 4; i <= 13; i++){
-    Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, ClassConsole::console_str[i]);
+    // drawing fps
+    ClassConsole::fps++;
+    ClassConsole::t = glutGet(GLUT_ELAPSED_TIME);
+    if (ClassConsole::t - ClassConsole::dt > 1000){
+        ClassConsole::console_str[0] = "FPS:" + Int_To_Str((int)(ClassConsole::fps * 1000.0 / (ClassConsole::t - ClassConsole::dt)));
+        ClassConsole::dt = ClassConsole::t;
+        ClassConsole::fps = 0;
+    }
+    ClassConsole::str_current_pos = ClassConsole::str_start_pos;
+    Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, ClassConsole::console_str[0]);
     ClassConsole::str_current_pos -= ClassConsole::str_height;
-  }
 
-  Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, (ClassConsole::command_str + ClassConsole::current_key + "_"));
+    // drawing coordinates
+    ClassConsole::console_str[1] = "X: ";
+    ClassConsole::console_str[1] += Int_To_Str(Object.xpos);
+    ClassConsole::console_str[1] += "; Y: ";
+    ClassConsole::console_str[1] += Int_To_Str(Object.ypos);
+    Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, ClassConsole::console_str[1]);
+    ClassConsole::str_current_pos -= ClassConsole::str_height;
 
-  glFlush();
+    for (unsigned short int i = 4; i <= 13; i++){
+        Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, ClassConsole::console_str[i]);
+        ClassConsole::str_current_pos -= ClassConsole::str_height;
+    }
 
-  glutSetWindow(wnd);
+    Object.DrawStaticString(-0.99, ClassConsole::str_current_pos, 0.0, GLUT_BITMAP_8_BY_13, (ClassConsole::command_str + ClassConsole::current_key + "_"));
+
+    glFlush();
+
+    glutSetWindow(wnd);
 }
 
 // set 2d mode
 void ClassConsole::Reshape(GLsizei Width, GLsizei Height){
-  glMatrixMode(GL_PROJECTION);
-  glLoadIdentity();
-  glViewport(0, 0, Width, Height);
-  gluOrtho2D(0, 0, Width, Height);
-  glMatrixMode(GL_MODELVIEW);
+    glMatrixMode(GL_PROJECTION);
+    glLoadIdentity();
+    glViewport(0, 0, Width, Height);
+    gluOrtho2D(0, 0, Width, Height);
+    glMatrixMode(GL_MODELVIEW);
 }
 
 // pressing Enter
 void ClassConsole::Enter(){
-  // moving console strings up
-  for (unsigned short int i = 4; i <= 12; i++){
-    ClassConsole::console_str[i] = ClassConsole::console_str[i + 1];
-  }
-  // running entered to console command
-  unsigned short int i = 0;
-  unsigned short int len = ClassConsole::current_key.length();
-  string command = "";
-  while ((ClassConsole::current_key[i] != ' ') && (i < len)){
-    command += ClassConsole::current_key[i];
-    i++;
-  }
-  i++;
-  string param1 = "";
-  while ((ClassConsole::current_key[i] != ' ') && (i < len)){
-    param1 += ClassConsole::current_key[i];
-    i++;
-  }
-  i++;
-  string param2 = "";
-  while ((ClassConsole::current_key[i] != ' ') && (i < len)){
-    param2 += ClassConsole::current_key[i];
-    i++;
-  }
-  // preparing to call function
-  bool valid1 = false;
-  bool valid2 = false;
-  int intparam1 = Str_To_Int(param1, &valid1);
-  int intparam2 = Str_To_Int(param2, &valid2);
-  ClassConsole::console_str[13] = "error command | ";
-  // call function with two parameters
-  if (valid1 && valid2){
-    if (ClassConsole::dualcommands.count(command) > 0){
-      (this->*ClassConsole::dualcommands[command])(intparam1, intparam2);
-      ClassConsole::console_str[13] = "done command | ";
+    // moving console strings up
+    for (unsigned short int i = 4; i <= 12; i++){
+        ClassConsole::console_str[i] = ClassConsole::console_str[i + 1];
     }
-  }
-  // call function with one parameter
-  if (valid1 && !valid2){
-    if (ClassConsole::singlecommands.count(command) > 0){
-      (this->*ClassConsole::singlecommands[command])(intparam1);
-      ClassConsole::console_str[13] = "done command | ";
+    // running entered to console command
+    unsigned short int i = 0;
+    unsigned short int len = ClassConsole::current_key.length();
+    string command = "";
+    while ((ClassConsole::current_key[i] != ' ') && (i < len)){
+        command += ClassConsole::current_key[i];
+        i++;
     }
-  }
-  // saving entered value to down string
-  ClassConsole::console_str[13] += ClassConsole::current_key;
-  // clear entering string
-  ClassConsole::current_key = "";
+    i++;
+    string param1 = "";
+    while ((ClassConsole::current_key[i] != ' ') && (i < len)){
+        param1 += ClassConsole::current_key[i];
+        i++;
+    }
+    i++;
+    string param2 = "";
+    while ((ClassConsole::current_key[i] != ' ') && (i < len)){
+        param2 += ClassConsole::current_key[i];
+        i++;
+    }
+    // preparing to call function
+    bool valid1 = false;
+    bool valid2 = false;
+    int intparam1 = Str_To_Int(param1, &valid1);
+    int intparam2 = Str_To_Int(param2, &valid2);
+    ClassConsole::console_str[13] = "error command | ";
+    // call function with two parameters
+    if (valid1 && valid2){
+        if (ClassConsole::dualcommands.count(command) > 0){
+            (this->*ClassConsole::dualcommands[command])(intparam1, intparam2);
+            ClassConsole::console_str[13] = "done command | ";
+        }
+    }
+    // call function with one parameter
+    if (valid1 && !valid2){
+        if (ClassConsole::singlecommands.count(command) > 0){
+            (this->*ClassConsole::singlecommands[command])(intparam1);
+            ClassConsole::console_str[13] = "done command | ";
+        }
+    }
+    // saving entered value to down string
+    ClassConsole::console_str[13] += ClassConsole::current_key;
+    // clear entering string
+    ClassConsole::current_key = "";
 }
 
 // goto x and y coordinates
 void ClassConsole::Goto_x_y(int x, int y){
-  std::cout << x;
-  std::cout << ";";
-  std::cout << y;
-  std::cout << ";\n";
+    std::cout << x;
+    std::cout << ";";
+    std::cout << y;
+    std::cout << ";\n";
 }
 
 // destructor
 ClassConsole::~ClassConsole(){
-  ClassConsole::dualcommands.clear();
-  ClassConsole::singlecommands.clear();
+    ClassConsole::dualcommands.clear();
+    ClassConsole::singlecommands.clear();
 }
--- engine.h
+++ engine.h
@@ -6,91 +6,92 @@ using namespace std;
 const float scale = 0.07;
 
 /*#######################   structures   #######################*/
-struct sector_coordinates{
-  int x;
-  int y;
+struct intPoint2d{
+    int x;
+    int y;
 };
 
 /*#######################   classes   #######################*/
 class ClassScene{
-  private:
-  public:
-    unsigned short int textures_count;
-    // index texture of tiles
-    GLuint *texture_tiles;
-    // array for loading textures
-    map <string, unsigned short int> TextureManager;
-    // coordinates of tiles
-    int xpos;
-    int ypos;
+    private:
+        void DrawStars();
+    public:
+        unsigned short int textures_count;
+        // index texture of tiles
+        GLuint *texture_tiles;
+        // array for loading textures
+        map <string, unsigned short int> TextureManager;
+        // coordinates of tiles
+        int xpos;
+        int ypos;
 
-    float camera_xpos = 0.0;
-    float camera_ypos = 0.0;
+        float camera_xpos = 0.0;
+        float camera_ypos = 0.0;
 
-    // initialization count of textures
-    void InitTextures(unsigned short int textures_count);
-    // loading texture by filename
-    void LoadTextureImage(const char *texture_name, GLuint texture);
-    // enter to ortho mode
-    void setOrthoProjection(GLsizei Width, GLsizei Height);
-    // exit of ortho mode
-    void setPerspectiveProjection();
-    // output string
-    void DrawStaticString(float x, float y, float z, void *font, string input);
-    // repainting OpenGL by reshape window
-    void Reshape(GLsizei Width, GLsizei Height);
-    // loading textures
-    void LoadTextures(vector<string> textures_list);
-    // deleting textures
-    void ClearTextures();
-    // painting Scene
-    void Draw();
-    /*processing move camera
-      variable direction:
-      0 - left
-      1 - right
-      2 - up
-      3 - down*/
-    void ProcessMoving(unsigned short int direction);
-    // destructor
-    ~ClassScene();
+        // initialization count of textures
+        void InitTextures(unsigned short int textures_count);
+        // loading texture by filename
+        void LoadTextureImage(const char *texture_name, GLuint texture);
+        // enter to ortho mode
+        void setOrthoProjection(GLsizei Width, GLsizei Height);
+        // exit of ortho mode
+        void setPerspectiveProjection();
+        // output string
+        void DrawStaticString(float x, float y, float z, void *font, string input);
+        // repainting OpenGL by reshape window
+        void Reshape(GLsizei Width, GLsizei Height);
+        // loading textures
+        void LoadTextures(vector<string> textures_list);
+        // deleting textures
+        void ClearTextures();
+        // painting Scene
+        void Draw();
+        /*processing move camera
+          variable direction:
+          0 - left
+          1 - right
+          2 - up
+          3 - down*/
+        void ProcessMoving(unsigned short int direction);
+        // destructor
+        ~ClassScene();
 
-    // friendly classes
-    friend class ClassConsole;
+        // friendly classes
+        friend class ClassConsole;
 };
 
 class ClassConsole{
-  public:
-    // array of functions
-    map <string, void (ClassConsole::*)(int, int)> dualcommands;
-    map <string, void (ClassConsole::*)(int)> singlecommands;
-    // string placing parameters
-    float str_height = 0.13;
-    float str_start_pos = 0.90;
-    float str_current_pos = 0.90;
-    // fps calculating parameters
-    int fps;
-    long t, dt;
-    // console strings
-    array<string, 14> console_str;
-    // entering string
-    string command_str = "Command~:";
-    string current_key = "";
-    // console visible state
-    bool visible = false;
+    public:
+        // array of functions
+        map <string, void (ClassConsole::*)(int, int)> dualcommands;
+        map <string, void (ClassConsole::*)(int)> singlecommands;
+        // string placing parameters
+        float str_height = 0.13;
+        float str_start_pos = 0.90;
+        float str_current_pos = 0.90;
+        // fps calculating parameters
+        int fps;
+        long t, dt;
+        // console strings
+        array<string, 14> console_str;
+        // entering string
+        string command_str = "Command~:";
+        string current_key = "";
+        // console visible state
+        bool visible = false;
 
-    // constructor
-    ClassConsole();
-    // painting Console
-    void Draw(ClassScene &object, unsigned short int console, unsigned short int wnd);
-    // set 2d mode
-    void Reshape(GLsizei Width, GLsizei Height);
-    // pressing Enter
-    void Enter();
-    // goto x y coordinates
-    void Goto_x_y(int x, int y);
-    // destructor
-    ~ClassConsole();
+        // constructor
+        ClassConsole();
+        // painting Console
+        void Draw(ClassScene &object, unsigned short int console, unsigned short int wnd);
+        // set 2d mode
+        void Reshape(GLsizei Width, GLsizei Height);
+        // pressing Enter
+        void Enter();
+        // goto x y coordinates
+        void Goto_x_y(int x, int y);
+        // destructor
+        ~ClassConsole();
 };
 
 #endif
